af607149c67842491dbc1272b6f36a17e9ab72d7
dpavankumar1301 <dpavankumar1301@gmail.com>
2023-06-07 22:33:03 +0530

    java script file created

---
+++ main.js
@@ -0,0 +1,13 @@
+var name1 = prompt("Enter your name") ;
+var name2 = prompt("Enter your crush name"); 
+
+var n = Math.random()
+var Lovecalculator = n*100 
+var lovescore = Math.floor(Lovecalculator) + 1
+
+if (lovescore > 70){
+    alert("your love score is "+lovescore+" you love each other")
+}  
+else{
+    alert("your love score is "+lovescore)
+}
